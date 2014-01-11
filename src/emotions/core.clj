b459ca531ae5b56e7bf06991750798432dc379d4
Dave Snowdon <dave@davesnowdon.com>
2014-01-11 14:30:31 +0000

    Implemented function to add value from percept to motivation

--- src/emotions/core.clj
+++ src/emotions/core.clj
@@ -1,6 +1,19 @@
 (ns emotions.core)
 
+;; motivations are maps
+
+;; satisfaction vectors are maps with the motivation name as a key
+
+;; percepts are maps with a key :satisfaction-vector
+
 (defn decay
-  ""
+  "Decay a motivation's current desire by the amount of its decay rate"
   [motivation]
-  (assoc motivation :desire (+ (:desire motivation) (:decay motivation))))
+  (assoc motivation :desire (+ (:desire motivation) (:decay-rate motivation))))
+
+(defn add-percept
+  "Add the from a percept's satisfaction vector with the motivations current desire"
+  [motivation percept]
+  (if-let [percept-score ((:name motivation) (:satisfaction-vector percept))]
+    (assoc motivation :desire (+ (:desire motivation) percept-score))
+    motivation))
--- test/emotions/core_test.clj
+++ test/emotions/core_test.clj
@@ -2,6 +2,11 @@
   (:require [emotions.core :refer :all]
             [expectations :refer :all]))
 
-(def hunger {:desire 0.0 :decay 0.1})
+(def hunger {:name :hunger, :desire 0.0, :decay-rate 0.1})
 
-(expect 0.1 (:desire (decay hunger)))
+(def percept {:satisfaction-vector {:hunger 0.5}})
+
+;; desire should increase by value of :decay-rate
+(expect 0.1 (:desire (decay {:desire 0.0, :decay-rate 0.1})))
+
+(expect 0.5 (:desire (add-percept hunger percept)))
