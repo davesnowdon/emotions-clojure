f3d870c21aa28f97cbf074f48ab63f676359e81c
Dave Snowdon <dave@davesnowdon.com>
2014-01-11 22:28:38 +0000

    Tidy up tests by using expect in

--- test/emotions/core_test.clj
+++ test/emotions/core_test.clj
@@ -8,7 +8,7 @@
 (def percept {:satisfaction-vector {:hunger 0.5, :survival 0.0}})
 
 ;; desire should increase by value of :decay-rate
-(expect 0.1 (:desire (decay-motivation {:desire 0.0, :decay-rate 0.1})))
+(expect {:desire 0.1} (in (decay-motivation {:desire 0.0, :decay-rate 0.1})))
 
 ;; decay all motivations should decay each motivation in a sequence
 (expect [{:name :hunger, :desire 0.1, :decay-rate 0.1, :max-change 0.3}]
@@ -16,40 +16,40 @@
 
 ;; adding a percept should modify the motivations desire by the value in the
 ;; percept's satisfaction vector the corresponds to the motivation
-(expect 0.5 (:desire (add-percept hunger percept)))
+(expect {:desire 0.5} (in (add-percept hunger percept)))
 
 ;; add-percepts should add all percepts to all motivations
 (expect [{:name :hunger, :desire 0.5, :decay-rate 0.1, :max-change 0.3}]
         (add-percepts [hunger] [percept]))
 
 ;; check that we save the value of desire when starting an update
-(expect 0.5 (:last-desire (first (start-update [{:desire 0.5}]))))
+(expect {:last-desire 0.5} (in (first (start-update [{:desire 0.5}]))))
 
 ;; desire should not be changed if it is within range
-(expect 0.5 (:desire (limit-desire-to-range {:desire 0.5
-                                             :min-desire 0.0
-                                             :max-desire 1.0})))
+(expect {:desire 0.5} (in (limit-desire-to-range {:desire 0.5
+                                                  :min-desire 0.0
+                                                  :max-desire 1.0})))
 
 ;; desire should not be allowed to be greater than max-desire
-(expect 0.8 (:desire (limit-desire-to-range {:desire 1.0
-                                             :min-desire 0.1
-                                             :max-desire 0.8})))
+(expect {:desire 0.8} (in (limit-desire-to-range {:desire 1.0
+                                                  :min-desire 0.1
+                                                  :max-desire 0.8})))
 
 ;; desire should not be allowed to be less than min-desire
-(expect 0.1 (:desire (limit-desire-to-range {:desire 0.0
-                                             :min-desire 0.1
-                                             :max-desire 0.8})))
+(expect {:desire 0.1} (in (limit-desire-to-range {:desire 0.0
+                                                  :min-desire 0.1
+                                                  :max-desire 0.8})))
 
 ;; if motivation does not define :min-desire default of 0.0 should be used
-(expect 0.0 (:desire (limit-desire-to-range {:desire -1})))
+(expect {:desire 0.0} (in (limit-desire-to-range {:desire -1})))
 
 ;; if motivation does not define :max-desire default of 1.0 should be used
-(expect 1.0 (:desire (limit-desire-to-range {:desire 2})))
+(expect {:desire 1.0} (in (limit-desire-to-range {:desire 2})))
 
 ;; desire should not change if it has changed less than :max-change
-(expect 0.5 (:desire (limit-desire-change {:desire 0.5
-                                           :last-desire 0.4
-                                           :max-change 0.3})))
+(expect {:desire 0.5} (in (limit-desire-change {:desire 0.5
+                                                :last-desire 0.4
+                                                :max-change 0.3})))
 
 ;; desire should be limited to not increasing by more than :max-change
 (expect #(float= 0.7 %)
