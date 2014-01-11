dc86a3df200478b65be65fdb5a9968ce3c985b8e
Dave Snowdon <dave@davesnowdon.com>
2014-01-11 12:55:44 +0000

    New versions of expectations

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [expectations "1.4.52"]]
-  :profile {:dev {:dependencies [[expectations "1.4.52"]]}}
+                 [expectations "1.4.56"]]
+  :profile {:dev {:dependencies [[expectations "1.4.56"]]}}
   :plugins [[lein-autoexpect "1.0"]
             [lein-ancient "0.5.4"]])
