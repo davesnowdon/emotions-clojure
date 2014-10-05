934832070d4468c243bf1f126dd47b3a5d8ab515
Dave Snowdon <dave@davesnowdon.com>
2014-10-05 13:30:29 +0100

    bump version to reflect upcoming changes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject emotions "0.1.4"
+(defproject emotions "0.2.0"
   :description "Clojure implementation of an emotional model for autonomous agents"
   :url "https://github.com/davesnowdon/emotions-clojure"
   :license {:name "LGPL v2.1"
