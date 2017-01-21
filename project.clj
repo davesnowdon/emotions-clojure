938a06efb20ee5ef03d4c47e2bd438fdd03c20a3
Dave Snowdon <dave@davesnowdon.com>
2017-01-21 13:03:52 +0000

    bumped dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/davesnowdon/emotions-clojure"
   :license {:name "LGPL v2.1"
             :url "http://www.gnu.org/licenses/lgpl-2.1.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-time "0.7.0"]
-                 [expectations "1.4.56"]]
-  :profile {:dev {:dependencies [[expectations "1.4.56"]]}}
-  :plugins [[lein-autoexpect "1.0"]
-            [lein-ancient "0.5.4"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-time "0.13.0"]
+                 [expectations "2.1.9"]]
+  :profile {:dev {:dependencies [[expectations "2.1.9"]]}}
+  :plugins [[lein-autoexpect "1.9.0"]
+            [lein-ancient "0.6.10"]])
